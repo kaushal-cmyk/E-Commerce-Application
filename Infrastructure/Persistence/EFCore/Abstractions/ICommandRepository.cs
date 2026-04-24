b537416f7d9d9446e3d3ab6c462887415f98164a
Kaushal Adhikari <kaushaladhikari098@gmail.com>
2026-04-24 23:43:31 +0545

    Command Repository

--- Infrastructure/Persistence/EFCore/Abstractions/ICommandRepository.cs
+++ Infrastructure/Persistence/EFCore/Abstractions/ICommandRepository.cs
@@ -1,6 +1,31 @@
+using System.Collections.Generic;
+
 namespace ECommerce.Infrastructure.Persistance.EFCore.Abstractions;
 
-public class ICommandRepository
+public interface ICommandRepository<TEntity> where TEntity : class
 {
+    IEnumerable<TEntity> AddRange(IEnumerable<TEntity> entities);
+
+    TEntity Add(TEntity entity);
+
+    TEntity Update(TEntity entity);
+
+    void Remove(object id);
+
+    void Remove(object?[]? id);
+
+    void Remove(TEntity entity);
+
+    Task<IEnumerable<TEntity>> AddRangeAsync(
+        IEnumerable<TEntity> entities,
+        CancellationToken cancellationToken = default(CancellationToken));
+
+    Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken = default(CancellationToken));
+
+    Task<TEntity> UpdateAsync(TEntity entity, CancellationToken cancellationToken = default (CancellationToken));
     
+    Task RemoveAsync(object id, CancellationToken cancellationToken = default(CancellationToken));
+    Task RemoveAsync(object?[]? id, CancellationToken cancellationToken = default(CancellationToken));
+    Task RemoveAsync(TEntity entity, CancellationToken cancellationToken = default(CancellationToken));
+
 }
