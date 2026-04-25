e0e577bb1657a3fbecda59b88918ade9fd37c596
Kaushal Adhikari <kaushaladhikari098@gmail.com>
2026-04-25 20:45:38 +0545

    DbContext

--- Infrastructure/Persistence/EFCore/DependencyInjection.cs
+++ Infrastructure/Persistence/EFCore/DependencyInjection.cs
@@ -1,6 +1,45 @@
-namespace DefaultNamespace;
+using DefaultNamespace;
+using DefaultNamespace.Abstractions;
+using ECommerce.Infrastructure.Persistance.EFCore.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
-public class DependencyInjection
+namespace ECommerce.Infrastructure.Persistance.EFCore;
+
+public static class DependencyInjection
 {
-    
+    public static IServiceCollection AddInfrastructurePersistenceDependencies(
+        this IServiceCollection services, string? connectionString)
+    {
+        services.AddBaseInfrastructurePersistenceDependencies();
+
+        if (!string.IsNullOrWhiteSpace(connectionString))
+            services.AddEcomDbContext(connectionString);
+
+        services.AddRepositories();
+        return services;
+    }
+
+    private static IServiceCollection AddEcomDbContext(
+        this IServiceCollection services, string connectionString)
+    {
+        services.AddScoped<DbContext, EcomDBContext>()
+            .AddScoped(sp => sp.GetRequiredService<IDbContextFactory<EcomDBContext>>().CreateDbContext())
+            .AddScoped<IDbContextFactory<DbContext>, ApplicationDbContextFactory<DbContext, EcomDBContext>>()
+            .AddDbContextFactory<EcomDBContext>((sp, options) =>
+            {
+                options.UseLazyLoadingProxies()
+                    .UseSqlServer(connectionString)
+                    .AddInterceptors(sp.GetRequiredService<AuditInterceptor>());
+            }, lifetime: ServiceLifetime.Scoped);
+
+        return services;
+    }
+
+    private static IServiceCollection AddRepositories(
+        this IServiceCollection services)
+    {
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        return services;
+    }
 }
--- Infrastructure/Persistence/EFCore/EcomDBContext.cs
+++ Infrastructure/Persistence/EFCore/EcomDBContext.cs
@@ -1,6 +1,19 @@
-namespace DefaultNamespace;
+using ECommerce.Infrastructure.Persistance.EFCore;
+using Microsoft.EntityFrameworkCore;
 
-public class EcomDBContext
+namespace ECommerce.Infrastructure.Persistance.EFCore;
+
+public class EcomDBContext(DbContextOptions<EcomDBContext> options) : DbContext
 {
-    
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        base.ConfigureConventions(configurationBuilder);
+
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(DependencyInjection.EFCoreAssemblyReference);
+    }
 }
