f2a909c7735665981843a4c41693c94fbe9dab30
Kaushal Adhikari <kaushaladhikari098@gmail.com>
2026-05-07 14:45:45 +0545

    Dbcontext fix

--- Infrastructure/Persistence/EFCore/EcomDBContext.cs
+++ Infrastructure/Persistence/EFCore/EcomDBContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ECommerce.Infrastructure.Persistance.EFCore;
 
-public class EcomDBContext(DbContextOptions<EcomDBContext> options) : DbContext
+public class EcomDBContext(DbContextOptions<EcomDBContext> options) : DbContext(options)
 {
     protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
     {
@@ -16,4 +16,4 @@ public class EcomDBContext(DbContextOptions<EcomDBContext> options) : DbContext
 
         modelBuilder.ApplyConfigurationsFromAssembly(DependencyInjection.EFCoreAssemblyReference);
     }
-}
+}  
