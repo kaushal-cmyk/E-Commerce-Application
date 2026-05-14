af7c4b0bd62a1e2d228329d62ef6f43e45c90b6b
Kaushal Adhikari <kaushaladhikari098@gmail.com>
2026-05-14 19:37:41 +0545

    Added command repository

---
+++ Infrastructure/Persistence/EFCore/Repositories/Implementations/CommandRepository.cs
@@ -0,0 +1,27 @@
+﻿
+using ECommerce.Infrastructure.Persistance.EFCore.Repositories.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Infrastructure.Persistance.EFCore.Repositories.Implementations
+{
+    public class CommandRepository<TEntity, TKey> : ICommandRepository<TEntity, TKey>
+        where TEntity : class
+        where TKey : notnull
+    {
+        protected readonly DbContext _context;
+        protected readonly DbSet<TEntity> _dbSet;
+
+        public CommandRepository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+
+
+
+
+
+
+    }
+}
