7293e79988f2c0eaf86c8885118f3856f5a0661d
Kaushal Adhikari <kaushaladhikari098@gmail.com>
2026-05-16 00:53:49 +0545

    uow fix

--- Infrastructure/Persistence/EFCore/DependencyInjection.cs
+++ Infrastructure/Persistence/EFCore/DependencyInjection.cs
@@ -1,4 +1,6 @@
 using System.Reflection;
+using ECommerce.Infrastructure.Persistance.EFCore.Repositories.Abstractions;
+using ECommerce.Infrastructure.Persistance.EFCore.Repositories.Implementations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -11,11 +13,24 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(
         this IServiceCollection services, string connectionString)
     {
+
         services.AddDbContext<EcomDBContext>(options =>
-            options.UseSqlServer(connectionString));
+    options.UseSqlServer(connectionString));
+
+        services.AddScoped<DbContext>(sp =>
+            sp.GetRequiredService<EcomDBContext>());
+
 
         // services.AddScoped<IProductRepository, ProductRepository>();
-
+        services.AddRepositories();
         return services;
     }
+
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        return services;
+    }
+
+
 }
