57dfb12fcc03ae7416eb482fb645b87021cb26d7
Kaushal Adhikari <kaushaladhikari098@gmail.com>
2026-05-16 15:12:25 +0545

    Logged in User

--- Core/Application/Services/Implementations/LoggedInUserService.cs
+++ Core/Application/Services/Implementations/LoggedInUserService.cs
@@ -1,8 +1,30 @@
 ﻿
+using ECommerce.Core.Application.Services.Abstractions;
+using Microsoft.AspNetCore.Http;
+
 namespace ECommerce.Core.Application.Services.Implementations
 {
-    public class LoggedInUserService
+    public class LoggedInUserService : ILoggedInUserService
     {
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
+        public LoggedInUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public string GetCurrentUserIdentity()
+        {
+            return _httpContextAccessor.HttpContext?.User?.FindFirst("sub")?.Value ?? "System";
+        }
+
+        public string GetSignInName()
+        {
+            return _httpContextAccessor.HttpContext?.User?.FindFirst("name")?.Value ?? "System";
+        }
+
+        public string GetPersonName()
+        {
+            return _httpContextAccessor.HttpContext?.User?.FindFirst("given_name")?.Value ?? "System";
+        }
     }
 }
