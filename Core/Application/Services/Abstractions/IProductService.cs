7deb5e30f5cc7f53989f3b03352b445f4bdda4c1
Kaushal Adhikari <kaushaladhikari098@gmail.com>
2026-05-18 00:39:30 +0545

    Added IProduct Service

---
+++ Core/Application/Services/Abstractions/IProductService.cs
@@ -0,0 +1,15 @@
+﻿
+using ECommerce.Core.Application.Dtos;
+
+namespace ECommerce.Core.Application.Services.Abstractions
+{
+    public interface IProductService
+    {
+        Task<ProductDto?> GetByIdAsync(Guid id);
+        Task<IEnumerable<ProductDto>> GetAllAsync();
+        Task<ProductDto> CreateAsync(CreateProductDto request);
+        Task<ProductDto> UpdateAsync(UpdateProductDto request);
+        Task DeleteAsync(Guid id);
+
+    }
+}
---
+++ Core/Application/Services/Implementations/ProductService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Core.Application.Services.Implementations
+{
+    internal class ProductService
+    {
+    }
+}
