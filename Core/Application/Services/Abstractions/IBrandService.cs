5e0ec3c96523c0cc1d1a4425454a4bc730a19b02
Kaushal Adhikari <kaushaladhikari098@gmail.com>
2026-05-20 10:17:37 +0545

    Added Brand Service

--- Core/Application/Services/Abstractions/IBrandService.cs
+++ Core/Application/Services/Abstractions/IBrandService.cs
@@ -1,8 +1,14 @@
 ﻿
+using ECommerce.Core.Application.Dtos;
+
 namespace ECommerce.Core.Application.Services.Abstractions
 {
     public interface IBrandService
     {
-
+        Task<BrandDto?> GetBrand(Guid id);
+        Task<IEnumerable<BrandDto>> GetAllBrand();
+        Task<CreateBrandDto> CreateBrand(CreateBrandDto request);
+        //Task<UpdateBrandDto> UpdateBrand(UpdateBrandDto request);
+        //Task DeleteBrand(Guid id);
     }
 }
--- Core/Application/Services/Implementations/BrandService.cs
+++ Core/Application/Services/Implementations/BrandService.cs
@@ -1,8 +1,52 @@
 ﻿
+using AutoMapper;
+using ECommerce.Core.Application.Dtos;
+using ECommerce.Core.Application.Interface;
+using ECommerce.Core.Application.Interface.Repositories;
+using ECommerce.Core.Domain.Entities;
+
 namespace ECommerce.Core.Application.Services.Implementations
 {
     public class BrandService
     {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IRepository<Brand, Guid> _brandRepository;
+        private readonly IMapper _mapper;
+
+
+        public BrandService(
+            IUnitOfWork unitOfWork,
+            IRepository<Brand, Guid> brandRepository,
+            IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _brandRepository = brandRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<BrandDto?> GetBrand(Guid id)
+        {
+            var brand = await _brandRepository.GetByIdAsync(id);
+            return _mapper.Map<BrandDto>(brand);
+        }
+
+        public async Task<IEnumerable<BrandDto>> GetAddBrand()
+        {
+            var brand = await _brandRepository.GetAllAsync();
+            return _mapper.Map<IEnumerable<BrandDto>>(brand);
+        }
+
+        public async Task<CreateBrandDto> CreateBrand(CreateBrandDto brandDto)
+        {
+            var brand = Brand.Create(
+                name: brandDto.Name,
+                logoUrl: brandDto.LogoUrl);
+
+            await _brandRepository.AddAsync(brand);
+            await _unitOfWork.SaveChangesAsync();
+
+            return _mapper.Map<CreateBrandDto>(brand);
+        }
 
     }
 }
