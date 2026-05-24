7ef186eeeff7c4d5ff94cdfc334fb9008735298e
Kaushal Adhikari <kaushaladhikari098@gmail.com>
2026-05-24 22:39:53 +0545

    Added user address dtos

---
+++ Core/Application/Dtos/UserAddressDto.cs
@@ -0,0 +1,34 @@
+﻿namespace ECommerce.Core.Application.DTOs.UserAddress;
+
+public record UserAddressDto(
+    Guid Id,
+    string FullName,
+    string PhoneNumber,
+    string Street,
+    string City,
+    string? State,
+    string Country,
+    string PostalCode,
+    bool IsDefault
+);
+
+public record CreateUserAddressDto(
+    string UserId,
+    string FullName,
+    string PhoneNumber,
+    string Street,
+    string City,
+    string? State,
+    string Country,
+    string PostalCode
+);
+
+public record UpdateUserAddressDto(
+    string FullName,
+    string PhoneNumber,
+    string Street,
+    string City,
+    string? State,
+    string Country,
+    string PostalCode
+);
