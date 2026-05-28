de1ea0ca15dfc03c40e6c7e8d3ba6b7f6d352652
Kaushal Adhikari <kaushaladhikari098@gmail.com>
2026-05-28 16:07:32 +0545

    Auth Entities

---
+++ Core/Domain/Base/Entities/Authentication/User.cs
@@ -0,0 +1,67 @@
+﻿
+using ECommerce.Core.Domain.Common;
+
+namespace ECommerce.Core.Domain.Entities.Authentication
+{
+    public class User : FullAuditedAggregateRoot<Guid>
+    {
+
+        #region Field and Properties
+
+        public Guid StoreId { get; private set; }
+        public string Email { get; private set; }
+
+        public string PasswordHash { get; private set; }
+        public bool IsVerified { get; private set; }
+        public bool IsActive { get; private set; }
+
+        public DateTimeOffset? LastLoginAt { get; private set; }
+
+        #endregion
+
+
+        #region Constructors
+
+#nullable disable
+        protected User() { }
+#nullable enable
+
+        private User(Guid storeId, string email, string passwordHash)
+        {
+            StoreId = storeId;
+            Email = email;
+            PasswordHash = passwordHash;
+            IsVerified = false;
+        }
+        #endregion
+
+
+        #region Actions and Behaviours
+
+        public static User Create(Guid storeId, string email, string passwordHash)
+        {
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(email);
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(passwordHash);
+            return new User(storeId, email, passwordHash);
+        }
+
+        public void VerifyUser()
+        {
+            IsVerified = true;
+        }
+
+        public void ActivateUser()
+        {
+            IsActive = true;
+        }
+        public void DeactivateUser()
+        {
+            IsActive = false;
+        }
+        public void RecordLogin()
+        {
+            LastLoginAt = DateTimeOffset.Now;
+        }
+        #endregion
+    }
+}
