916450270baec12e8ecd31b1033268cd08bebb70
Kaushal Adhikari <kaushaladhikari098@gmail.com>
2026-05-28 17:35:25 +0545

    Added Domain Errors

---
+++ Core/Domain/Base/Errors/DomainErrors.cs
@@ -0,0 +1,43 @@
+﻿
+namespace ECommerce.Core.Domain.Errors
+{
+    public static class DomainErrors
+    {
+        public static class Authentication
+        {
+            public static class ErrorMessage
+            {
+
+            }
+
+            public static class Errors
+            {
+
+            }
+        }
+        public static class Product
+        {
+            public static class ErrorMessage
+            {
+
+            }
+
+            public static class Errors
+            {
+
+            }
+        }
+        public static class Brand
+        {
+            public static class ErrorMessage
+            {
+
+            }
+
+            public static class Errors
+            {
+
+            }
+        }
+    }
+}
