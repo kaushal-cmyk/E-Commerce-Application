62d0e76d7659ec3a2a67688e21992731f9e0ac62
Kaushal Adhikari <kaushaladhikari098@gmail.com>
2026-05-31 23:49:09 +0545

    Added authentication service

---
+++ Core/Application/Services/Abstractions/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerce.Core.Application.Services.Abstractions
+{
+    public interface IAuthService
+    {
+        Task<AuthResponseDto> LoginAsync(LoginRequestDto dto);
+        Task<AuthResponseDto> RegisterAsync(RegisterRequestDto dto);
+        Task<AuthResponseDto> RefreshTokenAsync(RefreshTokenRequestDto dto);
+        Task LogoutAsync(string userId);
+    }
+}
