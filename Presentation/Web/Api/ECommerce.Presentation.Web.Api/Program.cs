6305027a8c0628c134c55ee542f90457c1b0fc81
Kaushal Adhikari <kaushaladhikari098@gmail.com>
2026-06-05 23:35:25 +0545

    Added DI

--- Presentation/Web/Api/ECommerce.Presentation.Web.Api/Program.cs
+++ Presentation/Web/Api/ECommerce.Presentation.Web.Api/Program.cs
@@ -1,7 +1,11 @@
 using ECommerce.Core.Application.Mapping;
 using ECommerce.Core.Application.Services.Abstractions;
 using ECommerce.Core.Application.Services.Implementations;
-using ECommerce.Infrastructure.Persistance.EFCore;
+using ECommerce.Infrastructure.Persistence.EFCore;
+using ECommerce.Infrastructure.Persistence.EFCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +22,28 @@ builder.Services.AddAutoMapper(cfg => cfg.AddProfile<MappingConfig>());
 builder.Services.AddScoped<ILoggedInUserService, LoggedInUserService>();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<IBrandService, BrandService>();
+
+builder.Services.AddScoped<IHasher, Hasher>();
+builder.Services.AddScoped<IJwtTokenService, JwtTokenService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(configuration["Jwt:Key"]!)),
+            ValidateIssuer = true,
+            ValidIssuer = configuration["Jwt:Issuer"],
+            ValidateAudience = true,
+            ValidAudience = configuration["Jwt:Audience"],
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero
+        };
+    });
+
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddInfrastructure(connectionString);
