ff962a0824540878ecb17a5a0350da183fc655d7
Kaushal Adhikari <kaushaladhikari098@gmail.com>
2026-06-11 14:49:07 +0545

    Added default role

--- Infrastructure/Persistence/EFCore/Data/DbInitilizer.cs
+++ Infrastructure/Persistence/EFCore/Data/DbInitilizer.cs
@@ -1,9 +1,18 @@
 ﻿using ECommerce.Infrastructure.Persistence.EFCore.Abstractions;
+using ECommerce.Infrastructure.Persistence.EFCore.Options;
+using Microsoft.Extensions.Options;
 
 namespace ECommerce.Infrastructure.Persistence.EFCore.Data
 {
     public class DbInitilizer : IDbInitilizer
     {
+        private readonly IOptions<DefaultRolesAndUserConfigurationOptions> _config;
+
+        public DbInitilizer(
+            IOptions<DefaultRolesAndUserConfigurationOptions> config)
+        {
+            _config = config;
+        }
         public void Initilize()
         {
 
--- Infrastructure/Persistence/EFCore/Options/DefaultRolesAndUserConfigurationOptions.cs
+++ Infrastructure/Persistence/EFCore/Options/DefaultRolesAndUserConfigurationOptions.cs
@@ -1,8 +1,26 @@
-﻿
-namespace ECommerce.Infrastructure.Persistence.EFCore.Options
+﻿namespace ECommerce.Infrastructure.Persistence.EFCore.Options
 {
     public class DefaultRolesAndUserConfigurationOptions
     {
+        public const string DefaultUserAndRole = "DefaultUserAndRole";
+
+        public string DefaultSuperAdminUserName { get; set; } = string.Empty;
+
+        public string DefaultSuperAdminUserEmail { get; set; } = string.Empty;
+
+        public string DefaultSuperAdminPassword { get; set; } = string.Empty;
+
+        public string DefaultAdminUserName { get; set; } = string.Empty;
+
+        public string DefaultAdminUserEmail { get; set; } = string.Empty;
+
+        public string DefaultAdminPassword { get; set; } = string.Empty;
+
+        public string DefaultCustomerUserName { get; set; } = string.Empty;
+
+        public string DefaultCustomerEmail { get; set; } = string.Empty;
+
+        public string DefaultCustomerPassword { get; set; } = string.Empty;
 
     }
 }
