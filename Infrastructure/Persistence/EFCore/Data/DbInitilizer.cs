41b7d96d2282914bb7df4af6f81437800a1617d8
Kaushal Adhikari <kaushaladhikari098@gmail.com>
2026-06-11 15:17:24 +0545

    Added Data Seeding

--- Infrastructure/Persistence/EFCore/Data/DbInitilizer.cs
+++ Infrastructure/Persistence/EFCore/Data/DbInitilizer.cs
@@ -1,5 +1,9 @@
-﻿using ECommerce.Infrastructure.Persistence.EFCore.Abstractions;
+﻿using ECommerce.Core.Domain.Entities.Authentication;
+using ECommerce.Core.Domain.Enumerations;
+using ECommerce.Infrastructure.Persistence.EFCore.Abstractions;
+using ECommerce.Infrastructure.Persistence.EFCore.Authentication;
 using ECommerce.Infrastructure.Persistence.EFCore.Options;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
 namespace ECommerce.Infrastructure.Persistence.EFCore.Data
@@ -7,15 +11,42 @@ namespace ECommerce.Infrastructure.Persistence.EFCore.Data
     public class DbInitilizer : IDbInitilizer
     {
         private readonly IOptions<DefaultRolesAndUserConfigurationOptions> _config;
+        private readonly EcomDBContext _db;
+        private readonly Hasher _hasher;
 
         public DbInitilizer(
+            EcomDBContext db,
+            Hasher hasher,
             IOptions<DefaultRolesAndUserConfigurationOptions> config)
         {
+            _db = db;
+            _hasher = hasher;
             _config = config;
         }
         public void Initilize()
         {
+            try
+            {
+                if (_db.Database.GetPendingMigrations().Any())
+                {
+                    _db.Database.Migrate();
+                }
 
+                if (!_db.Users.Any())
+                {
+                    _db.Users.AddRange(
+                        User.Create("superadmin", Guid.Empty, "superadmin@ecommerce.com", _hasher.Hash(_config.Value.DefaultSuperAdminPassword), UserRole.SuperAdmin),
+                        User.Create("admin", Guid.Empty, "admin@ecommerce.com", _hasher.Hash(_config.Value.DefaultAdminPassword), UserRole.Admin),
+                        User.Create("customer", Guid.Empty, "customer@ecommerce.com", _hasher.Hash(_config.Value.DefaultCustomerPassword), UserRole.Customer)
+                    );
+                    _db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+                throw;
+            }
         }
     }
 }
