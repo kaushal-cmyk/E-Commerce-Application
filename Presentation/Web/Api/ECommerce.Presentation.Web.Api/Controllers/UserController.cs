4a4c8bdeb4504209c671e515f84e66e30ec980d4
Kaushal Adhikari <kaushaladhikari098@gmail.com>
2026-06-14 23:32:20 +0545

    Added User Controller

---
+++ Presentation/Web/Api/ECommerce.Presentation.Web.Api/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Presentation.Web.Api.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+
+    }
+}
