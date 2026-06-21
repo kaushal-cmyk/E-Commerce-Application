d0acbf2f5baf81fe377d6f4004dd15f17a164e05
Kaushal Adhikari <kaushaladhikari098@gmail.com>
2026-06-21 22:47:51 +0545

    Updated product controller

--- Presentation/Web/Api/ECommerce.Presentation.Web.Api/Controllers/ProductController.cs
+++ Presentation/Web/Api/ECommerce.Presentation.Web.Api/Controllers/ProductController.cs
@@ -17,44 +17,59 @@ namespace ECommerce.Presentation.Web.Api.Controllers
             _productService = productService;
         }
 
-
         [HttpGet]
         public async Task<IActionResult> GetAllProducts()
         {
-            var products = await _productService.GetAllProducts();
-            return Ok(products);
+            var result = await _productService.GetAllProducts();
+
+            if (!result.IsSuccess)
+                return BadRequest(result.Error);
+
+            return Ok(result.Value);
         }
 
         [HttpGet("{id:guid}")]
         public async Task<IActionResult> GetById(Guid id)
         {
-            var product = await _productService.GetProduct(id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-            return Ok(product);
+            var result = await _productService.GetProduct(id);
+
+            if (!result.IsSuccess)
+                return NotFound(result.Error);
+
+            return Ok(result.Value);
         }
 
         [HttpPost("create")]
         public async Task<IActionResult> Create([FromBody] CreateProductDto request)
         {
-            var product = await _productService.CreateProduct(request);
-            return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
+            var result = await _productService.CreateProduct(request);
+
+            if (!result.IsSuccess)
+                return BadRequest(result.Error);
+
+            return CreatedAtAction(nameof(GetById), new { id = result.Value!.Id }, result.Value);
         }
 
         [HttpPut("{id:guid}")]
         public async Task<IActionResult> Update(Guid id, [FromBody] UpdateProductDto request)
         {
-            //var dto = request with { Id = id };
-            await _productService.UpdateProduct(request);
+            var dto = request with { Id = id };
+            var result = await _productService.UpdateProduct(dto);
+
+            if (!result.IsSuccess)
+                return BadRequest(result.Error);
+
             return NoContent();
         }
 
         [HttpDelete("{id:guid}")]
         public async Task<IActionResult> Delete(Guid id)
         {
-            await _productService.DeleteProduct(id);
+            var result = await _productService.DeleteProduct(id);
+
+            if (!result.IsSuccess)
+                return NotFound(result.Error);
+
             return NoContent();
         }
     }
