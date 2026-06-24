40d4f1db3adb6fceb515cd2912abb382997b107a
Kaushal Adhikari <kaushaladhikari098@gmail.com>
2026-06-24 21:52:49 +0545

    Updated product controller

--- Presentation/Web/Api/ECommerce.Presentation.Web.Api/Controllers/ProductController.cs
+++ Presentation/Web/Api/ECommerce.Presentation.Web.Api/Controllers/ProductController.cs
@@ -1,76 +1,93 @@
 ﻿using ECommerce.Core.Application.Dtos;
 using ECommerce.Core.Application.Services.Abstractions;
+using ECommerce.Core.Domain.Errors;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ECommerce.Presentation.Web.Api.Controllers
+namespace ECommerce.Presentation.Web.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class ProductController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    [Authorize]
-    public class ProductController : ControllerBase
+    private readonly IProductService _productService;
+
+    public ProductController(IProductService productService)
     {
-        private readonly IProductService _productService;
+        _productService = productService;
+    }
 
-        public ProductController(IProductService productService)
+    [HttpGet]
+    public async Task<IActionResult> GetAllProducts()
+    {
+        var result = await _productService.GetAllProducts();
+
+        if (result.IsFailure)
+            return BadRequest(result.Error);
+
+        return Ok(result.Value);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var result = await _productService.GetProduct(id);
+
+        if (result.IsFailure)
+            return NotFound(result.Error);
+
+        return Ok(result.Value);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateProductDto request)
+    {
+        var result = await _productService.CreateProduct(request);
+
+        if (result.IsFailure)
         {
-            _productService = productService;
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> GetAllProducts()
-        {
-            var result = await _productService.GetAllProducts();
-
-            if (!result.IsSuccess)
-                return BadRequest(result.Error);
-
-            return Ok(result.Value);
-        }
-
-        [HttpGet("{id:guid}")]
-        public async Task<IActionResult> GetById(Guid id)
-        {
-            var result = await _productService.GetProduct(id);
-
-            if (!result.IsSuccess)
+            if (result.Error == DomainErrors.Product.Errors.BrandNotFound)
                 return NotFound(result.Error);
 
-            return Ok(result.Value);
+            return BadRequest(result.Error);
         }
 
-        [HttpPost("create")]
-        public async Task<IActionResult> Create([FromBody] CreateProductDto request)
+        return CreatedAtAction(
+            nameof(GetById),
+            new { id = result.Value.Id },
+            result.Value);
+    }
+
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> Update(Guid id, UpdateProductDto request)
+    {
+        var dto = request with { Id = id };
+
+        var result = await _productService.UpdateProduct(dto);
+
+        if (result.IsFailure)
         {
-            var result = await _productService.CreateProduct(request);
-
-            if (!result.IsSuccess)
-                return BadRequest(result.Error);
-
-            return CreatedAtAction(nameof(GetById), new { id = result.Value!.Id }, result.Value);
-        }
-
-        [HttpPut("{id:guid}")]
-        public async Task<IActionResult> Update(Guid id, [FromBody] UpdateProductDto request)
-        {
-            var dto = request with { Id = id };
-            var result = await _productService.UpdateProduct(dto);
-
-            if (!result.IsSuccess)
-                return BadRequest(result.Error);
-
-            return NoContent();
-        }
-
-        [HttpDelete("{id:guid}")]
-        public async Task<IActionResult> Delete(Guid id)
-        {
-            var result = await _productService.DeleteProduct(id);
-
-            if (!result.IsSuccess)
+            if (result.Error == DomainErrors.Product.Errors.NotFound ||
+                result.Error == DomainErrors.Product.Errors.BrandNotFound)
+            {
                 return NotFound(result.Error);
+            }
 
-            return NoContent();
+            return BadRequest(result.Error);
         }
+
+        return Ok(result.Value);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        var result = await _productService.DeleteProduct(id);
+
+        if (result.IsFailure)
+            return NotFound(result.Error);
+
+        return NoContent();
     }
 }
